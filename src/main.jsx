e11aa612784172759d8a1c1bcc373d81bc309259
adjiebramantya <adjiebramantya.ab@gmail.com>
2023-12-11 15:08:13 +0700

    delete dep

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SpeedInsights />
     <Provider store={store}>
       <App />
     </Provider>
