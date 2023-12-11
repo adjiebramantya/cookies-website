a5a31bc18f70c472277b76b65e59d2289fcacbdb
adjiebramantya <adjiebramantya.ab@gmail.com>
2023-12-11 15:02:25 +0700

    speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <SpeedInsights />
     <Provider store={store}>
       <App />
     </Provider>
